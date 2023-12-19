211cb93c104596229334ef8fd4d3f09696866ff1
Yoshino <2593905944z@gmail.com>
2023-12-19 23:16:20 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,7 +1,14 @@
 #!/bin/bash
 # check root
 goversion=go1.21.5.linux-amd64.tar.gz
-installdir=/usr/local/
+tar -zxvf=/usr/local/
 [[ $EUID -ne 0 ]] && echo -e "${red}错误：${plain} 必须使用root用户运行此脚本！\n" && exit 1
 apt install mercurial git gcc -y
 wget -P ${installdir} https://go.dev/dl/${goversion}
+cd ${installdir} && tar -zxvf ${goversion}
+mkdir /home/go ; cd /home/go ; mkdir bin src pkg
+echo "export GOROOT=/usr/local/go">>/etc/profile
+echo "export PATH=$GOROOT/bin:$PATH">>/etc/profile
+echo "export GOPATH=/home/go">>/etc/profile
+source /etc/profile
+go version
