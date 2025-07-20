b3b49e17e3632a16ca24c7fc114c7018eb1dc80b
Yoshino <2593905944z@gmail.com>
2025-07-20 20:37:52 +0800

    Update replace.sh

--- replace.sh
+++ replace.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 mv /etc/apt/sources.list /etc/apt/sources.list.backup
 wget -P /etc/apt/ https://raw.githubusercontent.com/Grandova/golangshortcut/main/sources.list
-apt update -y ; apt upgrade -y
+apt update -y && apt upgrade -y
