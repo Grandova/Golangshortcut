ff6c515532474d8e7039a6059fdb060b30226532
Yoshino <2593905944z@gmail.com>
2025-07-20 20:37:18 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,14 +1,41 @@
 #!/bin/bash
-# check root
-goversion=go1.21.5.linux-amd64.tar.gz
-installdir=/usr/local/
-[[ $EUID -ne 0 ]] && echo -e "${red}错误：${plain} 必须使用root用户运行此脚本！\n" && exit 1
-apt install mercurial git gcc -y
-wget -P ${installdir} https://go.dev/dl/${goversion}
-cd ${installdir} && tar -zxvf ${goversion}
-mkdir /home/go ; cd /home/go ; mkdir bin src pkg
-echo 'export GOROOT=/usr/local/go'>>/etc/profile
-echo 'export PATH=$GOROOT/bin:$PATH'>>/etc/profile
-echo 'export GOPATH=/home/go'>>/etc/profile
-source /etc/profile
-go version
+
+if [ "$EUID" -ne 0 ]; then
+    echo -e "\e[31m错误：必须使用 root 用户运行此脚本！\e[0m"
+    exit 1
+fi
+
+
+GO_VERSION=go1.21.5.linux-amd64.tar.gz
+INSTALL_DIR=/usr/local
+GO_URL="https://go.dev/dl/${GO_VERSION}"
+GO_ROOT=${INSTALL_DIR}/go
+GO_PATH=/home/go
+
+
+apt update && apt install -y mercurial git gcc wget tar
+
+
+echo "正在下载 Go 安装包..."
+wget -c -P "$INSTALL_DIR" "$GO_URL" || { echo "Go 下载失败！"; exit 1; }
+
+
+cd "$INSTALL_DIR" || exit
+tar -xzf "$GO_VERSION" || { echo "解压失败！"; exit 1; }
+rm -f "$GO_VERSION"
+
+
+mkdir -p "$GO_PATH"/{bin,src,pkg}
+
+
+grep -q 'GOROOT=' /etc/profile || echo "export GOROOT=${GO_ROOT}" >> /etc/profile
+grep -q 'GOPATH=' /etc/profile || echo "export GOPATH=${GO_PATH}" >> /etc/profile
+grep -q '$GOROOT/bin' /etc/profile || echo 'export PATH=$GOROOT/bin:$PATH' >> /etc/profile
+
+export GOROOT=${GO_ROOT}
+export GOPATH=${GO_PATH}
+export PATH=$GOROOT/bin:$PATH
+
+
+echo "Go 版本："
+go version || echo "Go 未正确安装！"
